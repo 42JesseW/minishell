de704cfe0eaeddd55b86aa287855b2524e6fc444
Jesse Van der wolf <jevan-de@f1r2s16.codam.nl>
2021-11-12 11:53:19 +0100

    Folder structure changes and made some structures for parser

---
+++ srcs/shell/shell_destroy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   shell_destroy.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jevan-de <jevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/12 11:16:26 by jevan-de      #+#    #+#                 */
+/*   Updated: 2021/11/12 11:16:26 by jevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	shell_destroy(t_shell **shell)
+{
+	t_shell	*p;
+
+	if (!shell)
+		return ;
+	p = *shell;
+	if (p)
+	{
+		env_lst_del(&p->environ);
+		free(p);
+	}
+	*shell = NULL;
+}
